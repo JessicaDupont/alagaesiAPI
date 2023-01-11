ae39701c9992eb168da44dd5b87390726e89333e
Jessica Dupont <jessica.dupont1990@gmail.com>
2023-01-11 16:12:20 +0100

    structure ok

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ var express = require('express');
 var app = express();
 
 const hostname = '127.0.0.1';
-const port = process.env.PORT;
+const port = process.env.PORT || process.env.PORT_LOCAL;
 
-app.get('/', function(req, res){
-  res.send('Hello World')
-});
+const router = require('./routers/router');
+app.use(router);
 
 app.listen(port, ()=>{
   console.log(`Server running at http://${hostname}:${port}/`);
